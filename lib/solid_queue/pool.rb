b2bea1cbfa6180434d38e462296c6a0d977d8d07
Yusuke Sangenya <longinus.eva@gmail.com>
2026-03-03 23:32:04 +0900

    Fix

--- lib/solid_queue/pool.rb
+++ lib/solid_queue/pool.rb
@@ -8,11 +8,31 @@ module SolidQueue
 
     delegate :shutdown, :shutdown?, :wait_for_termination, to: :executor
 
+    class SolidExecutor < Concurrent::ThreadPoolExecutor
+      def initialize(options)
+        @on_idle = options.delete(:on_idle)
+        super(options)
+      end
+
+      def claim_size
+        self.synchronize { max_queue - (queue_length + active_count) }
+      end
+
+      def immediately_executable_count
+        self.synchronize { max_length - active_count }
+      end
+
+      def call_on_idle_if_claimable
+        return unless @on_idle
+
+        self.synchronize { @on_idle.call if queue_length <= 0 }
+      end
+    end  
+
     def initialize(thread_size:, extra_claim_size:, on_idle: nil)
       @thread_size = thread_size
       @extra_claim_size = extra_claim_size
       @on_idle = on_idle
-      @mutex = Mutex.new
     end
 
     def post(execution)
@@ -20,7 +40,7 @@ module SolidQueue
         wrap_in_app_executor do
           thread_execution.perform
         ensure
-          mutex.synchronize { on_idle.try(:call) if executor.queue_length <= executor.length }
+          executor.call_on_idle_if_claimable
         end
       end.on_rejection! do |e|
         handle_thread_error(e)
@@ -28,11 +48,11 @@ module SolidQueue
     end
 
     def claim_size
-      executor.max_queue - executor.queue_length
+      executor.max_queue - (executor.queue_length + executor.active_count)
     end
 
     def idle?
-      executor.queue_length < executor.length
+      executor.immediately_executable_count > 0
     end
 
     private
@@ -45,7 +65,7 @@ module SolidQueue
       }
 
       def executor
-        @executor ||= Concurrent::ThreadPoolExecutor.new DEFAULT_OPTIONS.merge(max_threads: thread_size, max_queue: thread_size + extra_claim_size)
+        @executor ||= SolidExecutor.new DEFAULT_OPTIONS.merge(max_threads: thread_size, max_queue: thread_size + extra_claim_size, on_idle: on_idle)
       end
   end
 end
